0de0162299eb6d45da0e6269283da3f994cd686f
Carolina Woodfine <carolina.woodfine@haaga-helia.fi>
2025-02-02 12:02:33 +0200

    JPA & database

--- src/main/java/project/bookstore/BookstoreApplication.java
+++ src/main/java/project/bookstore/BookstoreApplication.java
@@ -1,13 +1,35 @@
 package project.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import project.bookstore.domain.Book;
+import project.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner studentDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save books");
+			repository.save(new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", 10.99, 1925));
+			repository.save(new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084", 12.99, 1960));
+
+			log.info("fetch books");
+			for (Book book : repository.findAll()){
+				log.info(book.toString());
+			}
+		};
+}
 }
--- src/main/java/project/bookstore/web/BookController.java
+++ src/main/java/project/bookstore/web/BookController.java
@@ -1,5 +1,26 @@
 package project.bookstore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import project.bookstore.domain.BookRepository;
+
+
+@Controller
 public class BookController {
 
+    private BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/index")
+    public String booklist(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+    
 }
